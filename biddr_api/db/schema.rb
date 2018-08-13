a27aac90d717844a0bfef27d86b850a1e590b3bd
sarithapolisetty <sarithapolisetty14@gmail.com>
2018-08-13 10:10:48 -0700

    Created a bid model

--- biddr_api/db/schema.rb
+++ biddr_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_13_170559) do
+ActiveRecord::Schema.define(version: 2018_08_13_170951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,16 @@ ActiveRecord::Schema.define(version: 2018_08_13_170559) do
     t.index ["user_id"], name: "index_auctions_on_user_id"
   end
 
+  create_table "bids", force: :cascade do |t|
+    t.float "price"
+    t.bigint "auction_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["auction_id"], name: "index_bids_on_auction_id"
+    t.index ["user_id"], name: "index_bids_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -36,4 +46,6 @@ ActiveRecord::Schema.define(version: 2018_08_13_170559) do
   end
 
   add_foreign_key "auctions", "users"
+  add_foreign_key "bids", "auctions"
+  add_foreign_key "bids", "users"
 end
